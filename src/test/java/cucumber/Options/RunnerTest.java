24957b60054aaf9a6dccefc2680ddb674f3c66bc
Narendra <nspnsp2@gmail.com>
2021-11-17 19:47:57 +0530

    Udpated runner to run against TestNG
    Udpated runner to run against TestNG

--- src/test/java/cucumber/Options/RunnerTest.java
+++ src/test/java/cucumber/Options/RunnerTest.java
@@ -1,18 +1,18 @@
 package cucumber.Options;
 
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-//import io.cucumber.testng.AbstractTestNGCucumberTests;
-//import io.cucumber.testng.*;
-@RunWith(Cucumber.class)
+// import io.cucumber.junit.Cucumber;
+// import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.*;
+// @RunWith(Cucumber.class)
 @CucumberOptions(
 		features="src/test/java/features",
 		glue={"stepDefinitions"},
 		plugin="json:target/jsonReports/cucumber-report.json"
 //		tags="not @ignore"
 		)
-public class RunnerTest  {
+public class RunnerTest extends AbstractTestNGCucumberTests  {
 
 }
